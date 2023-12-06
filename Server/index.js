e032d2811e61ef513f436eeee119334d5d28e91d
tamimhasan19702 <tamimhasancu@gmail.com>
2023-12-07 01:04:36 +0600

    react installation in my public folder

--- Server/index.js
+++ Server/index.js
@@ -1,9 +1,27 @@
 /** @format */
 
 const express = require("express");
-const cors = requrie("cors");
+const cors = require("cors");
 const mongoose = require("mongoose");
 
 const app = express();
-
 require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("database connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const server = app.listen(process.env.PORT || 5000, () => {
+  console.log(`server is running on port ${process.env.PORT || 5000}`);
+});
