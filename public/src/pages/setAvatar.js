1505b91c58f6159bedf8a6cf8c462c3de4612ef7
tamimhasan19702 <tamimhasancu@gmail.com>
2024-02-18 16:12:27 +0600

    updated and fixed he buffer error on the website

--- public/src/pages/setAvatar.js
+++ public/src/pages/setAvatar.js
@@ -8,17 +8,124 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import axios from "axios";
 import { setAvatarRoute } from "../utils/APIRoutes";
+import { Buffer } from "buffer";
 
 export default function SetAvatar() {
   const api = "https://api.multiavatar.com/45678945";
   const navigate = useNavigate();
+  const [avatars, setAvatars] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [selectedAvatar, setSelectedAvatar] = useState(undefined);
+  const toastOptions = {
+    position: "bottom-right",
+    autoClose: 5000,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "dark",
+  };
+  const setProfilePicture = async () => {};
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const fetchedData = [];
+      for (let i = 0; i < 4; i++) {
+        const image = await axios.get(
+          `${api}/${Math.round(Math.random() * 1000)}`
+        );
+        console.log(image);
+        const buffer = new Buffer.from(image.data);
+        fetchedData.push(buffer.toString("base64"));
+      }
+      setAvatars(fetchedData);
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <>
-      <Container>SetAvatar</Container>
+      <Container>
+        <div className="title-container">
+          <h1>Pick an Avatar as a Profile Picture</h1>
+          <div className="avatars">
+            {avatars.map((avatar, index) => {
+              return (
+                <div
+                  key={index}
+                  className={`avatar ${
+                    selectedAvatar === index ? "selected" : ""
+                  }`}>
+                  <img
+                    src={`data:image/svg+xml;base64,${avatar}`}
+                    alt="avatar"
+                    onClick={() => setSelectedAvatar(index)}
+                  />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </Container>
       <ToastContainer />
     </>
   );
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 3rem;
+  background-color: #131324;
+  height: 100vh;
+  width: 100vw;
+
+  .loader {
+    max-inline-size: 100%;
+  }
+
+  .title-container {
+    h1 {
+      text-align: center;
+      color: white;
+      margin-bottom: 2rem;
+    }
+  }
+  .avatars {
+    display: flex;
+    gap: 2rem;
+
+    .avatar {
+      border: 0.4rem solid transparent;
+      padding: 0.4rem;
+      border-radius: 5rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      transition: 0.5s ease-in-out;
+      img {
+        height: 6rem;
+        transition: 0.5s ease-in-out;
+      }
+    }
+    .selected {
+      border: 0.4rem solid #4e0eff;
+    }
+  }
+  .submit-btn {
+    background-color: #4e0eff;
+    color: white;
+    padding: 1rem 2rem;
+    border: none;
+    font-weight: bold;
+    cursor: pointer;
+    border-radius: 0.4rem;
+    font-size: 1rem;
+    text-transform: uppercase;
+    &:hover {
+      background-color: #4e0eff;
+    }
+  }
+`;
