725cd248e64c67f910c30367ef9d4200baf75e3d
tamimhasan19702 <tamimhasancu@gmail.com>
2024-02-23 02:50:53 +0600

    update Contacts.js

--- public/src/components/Contacts.js
+++ public/src/components/Contacts.js
@@ -7,8 +7,64 @@ import Logo from "../assets/logo.svg";
 function Contacts({ contacts, currentUser }) {
   const [currentUserName, setCurrentUserName] = useState(undefined);
   const [currentUserImage, setCurrentUserImage] = useState(undefined);
-  useEffect(() => {}, [currentUser]);
-  return <div>Contacts</div>;
+  const [currentSelectedChat, setCurrentSelectedChat] = useState(undefined);
+  useEffect(() => {
+    if (currentUser) {
+      setCurrentUserImage(currentUser.avatarImage);
+      setCurrentUserName(currentUser.username);
+    }
+  }, [currentUser]);
+  const changeCurrentChat = (index, contact) => {
+    setCurrentSelectedChat(index);
+  };
+  return (
+    <>
+      {currentUserName && currentUserImage && (
+        <Container>
+          <div className="brand">
+            <img src={Logo} alt="logo" />
+            <h3>snappy</h3>
+          </div>
+          <div className="contacts">
+            {contacts.map((contact, index) => {
+              return (
+                <div
+                  className={
+                    index === currentSelectedChat
+                      ? "contact selected"
+                      : "contact"
+                  }
+                  key={index}>
+                  <div className="avatar">
+                    <img
+                      src={`data:image/svg+xml;base64,${contact.avatarImage}`}
+                      alt="avatar"
+                    />
+                  </div>
+                  <div className="username">
+                    <h3>{contact.username}</h3>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <div className="current-user">
+            <div className="avatar">
+              <img
+                src={`data:image/svg+xml;base64,${currentUser.avatarImage}`}
+                alt="avatar"
+              />
+            </div>
+            <div className="username">
+              <h3>{currentUser.username}</h3>
+            </div>
+          </div>
+        </Container>
+      )}
+    </>
+  );
 }
 
 export default Contacts;
+
+const Container = styled.div``;
