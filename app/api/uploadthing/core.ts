4b58fd66366106445a521d8a0e2e496c5e88ee25
tamimhasan19702 <tamimhasancu@gmail.com>
2024-03-01 23:23:02 +0600

    setting up uploadThing to upload images and files

---
+++ app/api/uploadthing/core.ts
@@ -0,0 +1,32 @@
+/** @format */
+
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { UploadThingError } from "uploadthing/server";
+import { auth } from "@clerk/nextjs";
+
+const f = createUploadthing();
+
+const handleAuth = () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    throw new Error("Unauthorized");
+  }
+  return { userId: userId };
+};
+
+export const ourFileRouter = {
+  serverImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
+    .middleware(() => handleAuth())
+    .onUploadComplete(() => {
+      console.log("Upload complete!");
+    }),
+
+  messageFile: f(["image", "pdf"])
+    .middleware(() => handleAuth())
+    .onUploadComplete((file) => {
+      console.log("Upload complete!", file);
+    }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
