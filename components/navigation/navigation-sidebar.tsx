559fb2680bca0777348fd369911abc864e838f0b
tamimhasan19702 <tamimhasancu@gmail.com>
2024-03-06 22:11:29 +0600

    added the plus icon as in the navigation menu

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -1,12 +1,36 @@
 /** @format */
 import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import NavigationAction from "./navigation-action";
+
 const NavigationSidebar = async () => {
   const profile = await currentProfile();
   if (!profile) {
     return redirect("/");
   }
-  return <div>NavigationSidebar</div>;
+
+  const servers = await db.server.findMany({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+  return (
+    <div className=" space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3">
+      <NavigationAction />
+    </div>
+  );
 };
 
 export default NavigationSidebar;
