b8abd60c5fa8b42fb4e1cd0a5ba7b0f57d227500
tamimhasan19702 <tamimhasancu@gmail.com>
2024-03-07 03:30:21 +0600

    updated the navigation Sidebar of the application

--- components/navigation/navigation-item.tsx
+++ components/navigation/navigation-item.tsx
@@ -3,8 +3,53 @@
 "use client";
 
 import React from "react";
-const NavigationItem = () => {
-  return <div>NavigationItem</div>;
+import Image from "next/image";
+import { useParams, useRouter } from "next/navigation";
+import { cn } from "@/lib/utils";
+import { ActionToolTip } from "../action-tooltip";
+
+interface NavigationItemProps {
+  id: string;
+  imageUrl: string;
+  name: string;
+}
+
+const NavigationItem = ({ id, imageUrl, name }: NavigationItemProps) => {
+  const params = useParams();
+  const router = useRouter;
+  const onClick = () => {
+    router
+      .push(`/servers/${id}`)
+      .catch((err) => console.warn("Failed to navigate:", err));
+  };
+  return (
+    <ActionToolTip side="right" align="start" label={name}>
+      <div>
+        <button onClick={onClick} className="group relative flex items-center">
+          <div
+            className={cn(
+              "absolute left-0 bg-primary rounded-r-full transition-all w-[4px]",
+              params?.serverId !== id && "group-hover:h-[20px]",
+              params?.serverId === id ? "h-[36px]" : "h-[8px]"
+            )}>
+            <div
+              className={cn(
+                "relative group flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden",
+                params?.serverId === id &&
+                  "bg-primary/10 text-primary rounded-[16px]"
+              )}>
+              <Image
+                fill={true}
+                src={imageUrl}
+                alt="Upload"
+                className="rounded-full"
+              />
+            </div>
+          </div>
+        </button>
+      </div>
+    </ActionToolTip>
+  );
 };
 
 export default NavigationItem;
--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -6,6 +6,8 @@ import NavigationAction from "./navigation-action";
 import { Separator } from "@/components/ui/separator";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import NavigationItem from "./navigation-item";
+import { ModeToggle } from "../mode-toggle";
+import { UserButton } from "@clerk/nextjs";
 
 const NavigationSidebar = async () => {
   const profile = await currentProfile();
@@ -29,10 +31,25 @@ const NavigationSidebar = async () => {
       <ScrollArea className="flex-1 w-full">
         {servers.map((server) => (
           <div key={server.id} className="mb-4">
-            <NavigationItem />
+            <NavigationItem
+              id={server.id}
+              imageUrl={server.imageUrl}
+              name={server.name}
+            />
           </div>
         ))}
       </ScrollArea>
+      <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
+        <ModeToggle />
+        <UserButton
+          afterSignOutUrl="/"
+          appearance={{
+            elements: {
+              avatarBox: "h-[48px] w-[48px]",
+            },
+          }}
+        />
+      </div>
     </div>
   );
 };
