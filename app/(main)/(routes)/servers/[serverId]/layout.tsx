a444918a838db2d7e1ecd50d741d5f5719b05506
tamimhasan19702 <tamimhasancu@gmail.com>
2024-03-08 21:18:35 +0600

    update on the serverSide component

--- app/(main)/(routes)/servers/[serverId]/layout.tsx
+++ app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -1,5 +1,6 @@
 /** @format */
 
+import ServerSidebar from "@/components/server/server-sidebar";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs";
@@ -35,7 +36,9 @@ const ServerIdlayout = async ({
 
   return (
     <div className="h-full">
-      <div className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0"></div>
+      <div className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0">
+        <ServerSidebar serverId={params.serverId} />
+      </div>
       <main className="h-full md:pl-60">{children}</main>
     </div>
   );
---
+++ components/server/server-sidebar.tsx
@@ -0,0 +1,67 @@
+/** @format */
+
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { ChannelType } from "@prisma/client";
+import { redirect } from "next/navigation";
+
+interface ServerHeaderProps {
+  serverId: string;
+}
+const ServerSidebar = async ({ serverId }: ServerHeaderProps) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirect("/");
+  }
+
+  const server = await db.server.findUnique({
+    where: {
+      id: serverId,
+    },
+    include: {
+      channel: {
+        orderBy: {
+          createdAt: "asc",
+        },
+      },
+      members: {
+        include: {
+          profile: true,
+        },
+        orderBy: {
+          role: "asc",
+        },
+      },
+    },
+  });
+
+  const textChannels = server?.channel.filter(
+    (channel) => channel.type === ChannelType.TEXT
+  );
+  const audioChannels = server?.channel.filter(
+    (channel) => channel.type === ChannelType.AUDIO
+  );
+  const videoChannels = server?.channel.filter(
+    (channel) => channel.type === ChannelType.VIDEO
+  );
+  const members = server?.members.filter(
+    (member) => member.profileId !== profile.id
+  );
+
+  if (!server) {
+    redirect("/");
+  }
+
+  const role = server.members.find(
+    (member) => member.profileId === profile.id
+  )?.role;
+
+  return (
+    <div className="flex flex-col h-full text-primary w-full dark:bg-[#2B2D31] bg-[#F2F3F5]">
+      ServerSidebar sa
+    </div>
+  );
+};
+
+export default ServerSidebar;
