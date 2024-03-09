6a456995a66f479b53646ad82a2a2e70e0d15bbe
tamimhasan19702 <tamimhasancu@gmail.com>
2024-03-10 01:47:47 +0600

    updated the server admin server and added all the necessary menu options

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -7,8 +7,17 @@ import {
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from "@radix-ui/react-dropdown-menu";
-import { ChevronDown } from "lucide-react";
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
+import {
+  ChevronDown,
+  LogOut,
+  PlusCircle,
+  Settings,
+  Trash,
+  UserPlus,
+  Users,
+} from "lucide-react";
 
 interface ServerHeaderProps {
   server: ServerWithMemberWithProfiles;
@@ -17,6 +26,7 @@ interface ServerHeaderProps {
 const ServerHeader = ({ server, role }: ServerHeaderProps) => {
   const isAdmin = role === MemberRole.ADMIN;
   const isModerator = isAdmin || role === MemberRole.MODERATOR;
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className="focus:outline-none" asChild>
@@ -26,7 +36,55 @@ const ServerHeader = ({ server, role }: ServerHeaderProps) => {
         </button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]">
-        {isModerator && <DropdownMenuItem>Invite People</DropdownMenuItem>}
+        {isModerator && (
+          <DropdownMenuItem
+            onClick={() => {}}
+            className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer">
+            Invite People
+            <UserPlus className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
+        )}
+        {isAdmin && (
+          <DropdownMenuItem
+            onClick={() => {}}
+            className="px-3 py-2 text-sm cursor-pointer">
+            Server Settings
+            <Settings className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
+        )}
+        {isAdmin && (
+          <DropdownMenuItem
+            onClick={() => {}}
+            className="px-3 py-2 text-sm cursor-pointer">
+            Manage Members
+            <Users className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
+        )}
+        {isModerator && (
+          <DropdownMenuItem
+            onClick={() => {}}
+            className="px-3 py-2 text-sm cursor-pointer">
+            Create Channel
+            <PlusCircle className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
+        )}
+        {isModerator && <DropdownMenuSeparator />}
+        {isAdmin && (
+          <DropdownMenuItem
+            onClick={() => {}}
+            className="px-3 py-2 text-sm cursor-pointer text-rose-500">
+            Delete Channel
+            <Trash className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
+        )}
+        {!isAdmin && (
+          <DropdownMenuItem
+            onClick={() => {}}
+            className="px-3 py-2 text-sm cursor-pointer text-rose-500">
+            Leave Server
+            <LogOut className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
+        )}
       </DropdownMenuContent>
     </DropdownMenu>
   );
