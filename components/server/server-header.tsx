e9243402561e30783f6c190006c1752c78138010
tamimhasan19702 <tamimhasancu@gmail.com>
2024-03-12 20:34:33 +0600

    updated the patch  route and now server updates the name

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -50,7 +50,9 @@ const ServerHeader = ({ server, role }: ServerHeaderProps) => {
         )}
         {isAdmin && (
           <DropdownMenuItem
-            onClick={() => {}}
+            onClick={() => {
+              onOpen("editServer", { server });
+            }}
             className="px-3 py-2 text-sm cursor-pointer">
             Server Settings
             <Settings className="h-4 w-4 ml-auto" />
