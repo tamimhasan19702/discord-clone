d798b12f930f70bfc39d031ef6dbba768815e79c
tamimhasan197020 <tamimhasancu@gmail.com>
2024-04-13 21:30:04 +0600

    update page.tsx

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,7 +1,35 @@
 /** @format */
 
-const ChannelIdPage = () => {
-  return <div>ChannelId Page</div>;
+import { currentProfile } from "@/lib/current-profile";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { db } from "@/lib/db";
+
+interface ChannelIdPageProps {
+  params: {
+    serverId: string;
+    channelId: string;
+  };
+}
+
+const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
+  const profile = await currentProfile();
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const channel = await db.member.findUnique({
+    where: {
+      id: params.channelId,
+    },
+  });
+
+  const member = await db.member.findFirst({
+    where: {
+      serverId: params.serverId,
+      profileId: profile.id,
+    },
+  });
+  return <div>Chan nelId Page</div>;
 };
 
 export default ChannelIdPage;
