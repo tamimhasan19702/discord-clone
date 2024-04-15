bede3c72b1dfa1c6a5f9d9dbe7d3d6630b08fa35
tamimhasan197020 <tamimhasancu@gmail.com>
2024-04-16 03:04:46 +0600

    update chat-header.tsx, mobile-toggle.tsx and schema.prisma

--- components/mobile-toggle.tsx
+++ components/mobile-toggle.tsx
@@ -21,7 +21,7 @@ const MobileToggle = ({ serverId }: { serverId: string }) => {
           <Menu />
         </Button>
       </SheetTrigger>
-      <SheetContent side="right" className="p-0 flex gap-0">
+      <SheetContent side="left" className="p-0 flex gap-0">
         <div className="w=[72px]">
           <NavigationSidebar />
         </div>
