5ef7d764ef3413f50232f317516d11c1ba20aed6
tamimhasan197020 <tamimhasancu@gmail.com>
2024-04-15 17:22:22 +0600

    build(deps): update chat-header.tsx and package-lock.json

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,5 +1,6 @@
 /** @format */
 import { Hash, Menu } from "lucide-react";
+import MobileToggle from "../mobile-toggle";
 interface ChatHeaderProps {
   serverId: string;
   name: string;
@@ -10,10 +11,11 @@ interface ChatHeaderProps {
 const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) => {
   return (
     <div className="text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
-      <Menu />
+      <MobileToggle />
       {type === "channel" && (
         <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
       )}
+      <p className="font-semibold text-md text-black dark:text-white">{name}</p>
     </div>
   );
 };
---
+++ components/mobile-toggle.tsx
@@ -0,0 +1,35 @@
+/** @format */
+
+import { Menu } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Button } from "./ui/button";
+
+const MobileToggle = () => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Are you absolutely sure?</SheetTitle>
+          <SheetDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </SheetDescription>
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileToggle;
