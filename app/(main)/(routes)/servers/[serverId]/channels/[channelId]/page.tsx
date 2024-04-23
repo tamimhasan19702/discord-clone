1e281aeab5639c8be84b78e12a02b26f53548973
tamimhasan197020 <tamimhasancu@gmail.com>
2024-04-23 20:41:53 +0600

    update page.tsx and tsconfig.json

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -5,6 +5,7 @@ import { redirectToSignIn } from "@clerk/nextjs";
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
 import ChatHeader from "@/components/chat/chat-header";
+import ChatInput from "@/components/chat/chat-input";
 
 interface ChannelIdPageProps {
   params: {
@@ -43,6 +44,8 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
         serverId={channel.serverId}
         type="channel"
       />
+      <div className="flex-1">Future Messages</div>
+      <div>hi</div>
     </div>
   );
 };
---
+++ components/chat/chat-input.tsx
@@ -0,0 +1,11 @@
+/** @format */
+
+"use client";
+
+import React from "react";
+
+function ChatInput() {
+  return <div>ChatInputdsdsdasassa</div>;
+}
+
+export default ChatInput;
