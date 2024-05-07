f7dfd4c257702d192b7fb11b19f5a3215e049242
tamimhasan197020 <tamimhasancu@gmail.com>
2024-05-07 23:32:11 +0600

    update chat-input.tsx, modal-provider.tsx, use-modal-store.ts and messages.ts

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -11,6 +11,7 @@ import { Input } from "../ui/input";
 import { Plus, Smile } from "lucide-react";
 import axios from "axios";
 import qs from "query-string";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ChatInputProps {
   apiUrl: string;
@@ -24,6 +25,8 @@ const formSchema = z.object({
 });
 
 function ChatInput({ apiUrl, query, name, type }: ChatInputProps) {
+  const { onOpen } = useModal();
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -58,7 +61,9 @@ function ChatInput({ apiUrl, query, name, type }: ChatInputProps) {
                 <div className="relative p-4 pb-6">
                   <button
                     type="button"
-                    onClick={() => {}}
+                    onClick={() => {
+                      onOpen("messageFile", { apiUrl, query });
+                    }}
                     className="absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-3000 transition rounded-full p-1 flex items-center justify-center">
                     <Plus className="text-white dark:text-[#313338]" />
                   </button>
