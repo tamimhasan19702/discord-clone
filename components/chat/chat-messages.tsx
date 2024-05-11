6be5f963df30e43a6e5e14c99acc63751fcb9fc2
tamimhasan197020 <tamimhasancu@gmail.com>
2024-05-12 00:45:10 +0600

    update page.tsx and chat-messages.tsx

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -1,6 +1,31 @@
 /** @format */
 
-const ChatMessages = () => {
+import { Member } from "@prisma/client";
+import { StringValidation } from "zod";
+
+interface ChatMessageProps {
+  name: string;
+  member: Member;
+  chatId: string;
+  apiUrl: string;
+  socketUrl: string;
+  socketQuery: Record<string, string>;
+  paramKey: "channelId" | "conversationId";
+  paramValue: string;
+  type: "channel" | "conversation";
+}
+
+const ChatMessages = ({
+  name,
+  member,
+  chatId,
+  apiUrl,
+  socketUrl,
+  socketQuery,
+  paramKey,
+  paramValue,
+  type,
+}: ChatMessageProps) => {
   return (
     <div>
       <h1>ChatMessages</h1>
